a703569f7950f71f0b15a19c2fb7d5a5208da80b
Mark Rodgers <markrodgers.dev@gmail.com>
2024-08-29 18:21:21 +0100

    Add placeShip() tests and method

--- src/scripts/gameboard.js
+++ src/scripts/gameboard.js
@@ -1,10 +1,14 @@
+import Ship from '../scripts/ship';
+
 class Gameboard {
   constructor() {
     this.board = this.#makeBoard();
     this.misses = [];
+    this.hits = [];
     this.ships = [];
   }
 
+  // Make a 10x10 grid to represent the board
   #makeBoard() {
     const board = [];
     for (let i = 0; i < 10; i++) {
@@ -15,6 +19,54 @@ class Gameboard {
       board.push(row);
     }
     return board;
+  }
+
+  //Check for out of bounds of board, ship positions going out of bounds and ship collisions
+  #isValidPlacement(shipLength, direction, [posY, posX]) {
+    const potentialPos = [];
+    if (posY < 0 || posY > 9 || posX < 0 || posX > 9) {
+      return false;
+    } else if (direction === 'horizontal') {
+      if (shipLength + posX > 10) {
+        return false;
+      }
+      for (let i = 0; i < shipLength; i++) {
+        potentialPos.push([posY, posX + i]);
+      }
+    } else if (direction === 'vertical') {
+      if (shipLength + posY > 10) {
+        return false;
+      }
+      for (let i = 0; i < shipLength; i++) {
+        potentialPos.push([posY + i, posX]);
+      }
+    }
+
+    return potentialPos.every(([posY, posX]) => this.board[posY][posX] === 0);
+  }
+
+  placeShip(shipLength, direction, [posY, posX]) {
+    if (this.#isValidPlacement(shipLength, direction, [posY, posX])) {
+      const ship = new Ship(shipLength);
+      const shipData = {
+        ship: ship,
+        pos: [],
+      };
+
+      if (direction === 'horizontal') {
+        for (let i = 0; i < shipLength; i++) {
+          this.board[posY][posX + i] = ship;
+          shipData.pos.push([posY, posX + i]);
+        }
+      } else if (direction === 'vertical') {
+        for (let i = 0; i < shipLength; i++) {
+          this.board[posY + i][posX] = ship;
+          shipData.pos.push([posY + i, posX]);
+        }
+      }
+
+      this.ships.push(shipData);
+    }
   }
 }
 
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,13 +1,13 @@
 import Gameboard from '../scripts/gameboard.js';
+import Ship from '../scripts/ship.js';
+
+let board = null;
+let emptyBoard = null;
 
 describe('Gameboard', () => {
-  test('Module exists', () => {
-    expect(Gameboard).toBeDefined();
-  });
-
-  test('Creates gameboard', () => {
-    const board = new Gameboard();
-    const emptyBoard = [
+  beforeEach(() => {
+    board = new Gameboard();
+    emptyBoard = [
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -19,9 +19,146 @@ describe('Gameboard', () => {
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     ];
+  });
+
+  afterEach(() => {
+    board = null;
+    emptyBoard = null;
+  });
+
+  test('Module exists', () => {
+    expect(Gameboard).toBeDefined();
+  });
+
+  test('Creates gameboard', () => {
     expect(board).toBeInstanceOf(Gameboard);
     expect(board.board).toEqual(emptyBoard);
     expect(board.misses).toEqual([]);
+    expect(board.hits).toEqual([]);
     expect(board.ships).toEqual([]);
+  });
+
+  test('Places ship onto gameboard horizontally', () => {
+    const submarine = new Ship(2);
+    const testBoard = [
+      [submarine, submarine, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+
+    board.placeShip(2, 'horizontal', [0, 0]);
+
+    expect(board.board).toEqual(testBoard);
+    expect(board.ships).toContainEqual({
+      ship: submarine,
+      pos: [
+        [0, 0],
+        [0, 1],
+      ],
+    });
+  });
+
+  test('Places ship onto gameboard vertically', () => {
+    const destroyer = new Ship(3);
+    const testBoard = [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, destroyer],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, destroyer],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, destroyer],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+
+    board.placeShip(3, 'vertical', [3, 9]);
+
+    expect(board.board).toEqual(testBoard);
+    expect(board.ships).toContainEqual({
+      ship: destroyer,
+      pos: [
+        [3, 9],
+        [4, 9],
+        [5, 9],
+      ],
+    });
+  });
+
+  test('Ship can be placed on the edge of the board', () => {
+    const patrolBoat = new Ship(1);
+    const testBoard = [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, patrolBoat],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+    board.placeShip(1, 'horizontal', [0, 9]);
+    expect(board.board).toEqual(testBoard);
+    expect(board.ships).toContainEqual({
+      ship: patrolBoat,
+      pos: [[0, 9]],
+    });
+  });
+
+  test("Doesn't place ship if starting co-ordinate is out of bounds of the board", () => {
+    board.placeShip(2, 'horizontal', [3, 15]);
+    expect(board.board).toEqual(emptyBoard);
+    expect(board.ships).toEqual([]);
+
+    board.placeShip(3, 'vertical', [-1, 3]);
+    expect(board.board).toEqual(emptyBoard);
+    expect(board.ships).toEqual([]);
+  });
+
+  test("Doesn't place ship if any ship co-ordinate would go out of bounds of the board", () => {
+    board.placeShip(2, 'horizontal', [0, 9]);
+    expect(board.board).toEqual(emptyBoard);
+    expect(board.ships).toEqual([]);
+
+    board.placeShip(5, 'vertical', [6, 0]);
+    expect(board.board).toEqual(emptyBoard);
+    expect(board.ships).toEqual([]);
+  });
+
+  test("Doesn't place ship if any ship co-ordinates would overlap any existing ships", () => {
+    const destroyer = new Ship(3);
+    const submarine = new Ship(2);
+    const testBoard = [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, destroyer, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, destroyer, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, destroyer, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ];
+    board.placeShip(3, 'vertical', [3, 3]);
+    board.placeShip(2, 'horizontal', [4, 2]);
+    expect(board.board).toEqual(testBoard);
+    expect(board.ships).not.toContainEqual({
+      ship: submarine,
+      pos: [
+        [4, 2],
+        [4, 3],
+      ],
+    });
   });
 });
